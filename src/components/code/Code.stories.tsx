b756fe4140866400e712121f6f9d40f83c1afd9f
Luan van der Westhuizen <6928171+luanvdw@users.noreply.github.com>
2021-07-29 14:07:56 +0200

    Added forwarding ref to the button element inside a code block (#240)

--- src/components/code/Code.stories.tsx
+++ src/components/code/Code.stories.tsx
@@ -1,4 +1,5 @@
 import { Code, CodeProps } from "./Code"
+import { useRef, useEffect } from "react"
 
 export const Default = ({ isInline, prefix, children }: CodeProps) => {
   if (isInline) {
@@ -46,3 +47,17 @@ export const Inline = () => (
     Create a <Code isInline>.env</Code> file with the following
   </p>
 )
+
+export const WithRef = () => {
+  const ref = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    ref.current.focus()
+  }, [])
+
+  return (
+    <Code prefix="$" ref={ref}>
+      git clone
+    </Code>
+  )
+}
--- src/components/code/Code.tsx
+++ src/components/code/Code.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { forwardRef, ForwardedRef, useRef } from "react"
 import cn from "classnames"
 
 import { Button } from "../button/Button"
@@ -15,67 +15,72 @@ interface BaseProps {
  * Discriminated Union to accomodate the following conditional prop logic
  * isInline?: boolean (Optional flag to render inline Code styles)
  * prefix?: string (Optional string to render with a non inline Code block)
+ * ref?: A React ref to attach to the rendered Code block's copy button
  */
 type ConditionalProps =
-  | { isInline?: true; prefix?: never }
-  | { isInline?: false; prefix?: string }
+  | { isInline?: true; prefix?: never; ref?: never }
+  | { isInline?: false; prefix?: string; ref?: ForwardedRef<HTMLButtonElement> }
 
 export type CodeProps = BaseProps & ConditionalProps
 
-export const Code = ({
-  isInline = false,
-  prefix,
-  children,
-  className,
-}: CodeProps) => {
-  const toasts = useToasts()
+export const Code = forwardRef<HTMLButtonElement, CodeProps>(
+  (
+    { isInline = false, prefix, children, className }: CodeProps,
+    forwardedRef
+  ) => {
+    const _ref = useRef<HTMLButtonElement>(null)
+    const ref = forwardedRef || _ref
 
-  const handleCodeCopy = async (): Promise<void> => {
-    try {
-      await navigator.clipboard.writeText(children)
-      toasts.add({
-        title: `Copied!`,
-        variant: "positive",
-      })
-    } catch (err) {
-      toasts.add({
-        title: `Command could not be copied`,
-        variant: "negative",
-      })
+    const toasts = useToasts()
+
+    const handleCodeCopy = async (): Promise<void> => {
+      try {
+        await navigator.clipboard.writeText(children)
+        toasts.add({
+          title: `Copied!`,
+          variant: "positive",
+        })
+      } catch (err) {
+        toasts.add({
+          title: `Command could not be copied`,
+          variant: "negative",
+        })
+      }
     }
-  }
 
-  // If no children are provided
-  if (!children) {
-    throw new Error("The Code component must be wrapped around a string")
-  }
+    // If no children are provided
+    if (!children) {
+      throw new Error("The Code component must be wrapped around a string")
+    }
 
-  return (
-    <span
-      className={cn(
-        "items-center rounded-lg",
-        "font-mono text-sm",
-        {
-          "inline-flex p-1 mx-1": isInline,
-          "bg-gray-200 text-gray-800": isInline,
-          "flex w-full justify-start py-3 px-4": !isInline,
-          "bg-gray-900 text-gray-100": !isInline,
-        },
-        className
-      )}
-    >
-      {prefix && <span className="mr-2 text-gray-500">{prefix}</span>}
-      {children}
-      {!isInline && (
-        <div className="relative flex-grow flex justify-end ml-2">
-          <Button
-            variant="secondary"
-            icon="copy"
-            iconSize="sm"
-            onPress={handleCodeCopy}
-          />
-        </div>
-      )}
-    </span>
-  )
-}
+    return (
+      <span
+        className={cn(
+          "items-center rounded-lg",
+          "font-mono text-sm",
+          {
+            "inline-flex p-1 mx-1": isInline,
+            "bg-gray-200 text-gray-800": isInline,
+            "flex w-full justify-start py-3 px-4": !isInline,
+            "bg-gray-900 text-gray-100": !isInline,
+          },
+          className
+        )}
+      >
+        {prefix && <span className="mr-2 text-gray-500">{prefix}</span>}
+        {children}
+        {!isInline && (
+          <div className="relative flex-grow flex justify-end ml-2">
+            <Button
+              ref={ref}
+              variant="secondary"
+              icon="copy"
+              iconSize="sm"
+              onPress={handleCodeCopy}
+            />
+          </div>
+        )}
+      </span>
+    )
+  }
+)

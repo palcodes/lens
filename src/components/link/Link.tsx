58b26621785e20824145155b9913c1a8721030d0
Siddhant <me@madebysid.com>
2021-08-19 06:55:14 +0200

    improve: allow rich children in `Link`s (#270)
    * improve: allow rich children in `Link`s
    
    * fix: inline link

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,22 +1,20 @@
-import React, { forwardRef, useRef } from "react"
+import React, { forwardRef, Props, useRef } from "react"
 import cn from "classnames"
 import { useHover } from "@react-aria/interactions"
 import { useLink } from "@react-aria/link"
 import { mergeProps } from "@react-aria/utils"
 import { FocusRing } from "../focus-ring/FocusRing"
 
-export type LinkProps = {
+export type LinkProps = React.PropsWithChildren<{
   /** An HTML ID attribute that will be attached to the the rendered component. Useful for targeting it from tests */
   id?: string
-  /** The visual text for the hyperlink */
-  children: string
   /** The location this Link points to */
   href: string
   /** Controls if this link is disabled */
   isDisabled?: boolean
   /** Controls if this link should open in a new tab */
   openInNewTab?: boolean
-}
+}>
 
 export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
   (
@@ -40,7 +38,8 @@ export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
           {...mergeProps(linkProps, hoverProps)}
           href={href}
           rel="noopener noreferrer"
-          className={cn("mx-1", "underline", {
+          className={cn("mx-1 underline", {
+            "flex space-x-2": typeof children !== "string", // If "rich" children are provided, lay them out correctly
             "text-gray-600 dark:text-gray-400": !isHovered && !isDisabled,
             "text-gray-500 dark:text-gray-300": isHovered,
             "text-gray-400 dark:text-gray-700": isDisabled,

877f6bf7c2c9cc2a603f1ec29dfc0fd38d2d13d1
Siddhant <me@madebysid.com>
2021-10-22 12:45:04 +0200

    fix: add `id` prop to `Code`

--- src/components/code/Code.tsx
+++ src/components/code/Code.tsx
@@ -1,54 +1,41 @@
 import React, { forwardRef, useState, useEffect } from "react"
 import cn from "classnames"
-
 import { Button } from "../button/Button"
-import { Icon } from "../icon/Icon"
 
-const COPIED_STATUS_TIMEOUT = 3000
-const DEFAULT_SECRET_TEXT = "************"
-
-enum CopiedStatus {
-  Success = "SUCCESS",
-  Failed = "FAILED",
-}
-
-interface CommonProps {
+export type CodeProps = {
+  /** An HTML ID attribute that will be attached to the the rendered component. Useful for targeting it from tests */
+  id?: string
   /** Text value to be styled as code like representation */
   value: string
-  /** prefix?: string (Optional string to render with a non inline Code block) */
+  /** Optional string to render before the `value`. Useful as a way to supply a PROMPT. */
   prefix?: string
   /** Additional classes that will be spread over the icon. Avoid changing the Icon visually. */
   className?: string
-}
-
-/**
- * Narrowing conditions, highlighting possible paths of execution
- * isSecret?: Controls if the Code component holds sensitive data that is not to be revealed immediately
- * secretText?: Optional masked value for sensitive data.
- * canReveal?: Stipulates if the user has permission to reveal sensitive data or not
- */
-type ConditionalProps =
+} & (
+  | {
+      /** Controls if the Code component holds sensitive data that is not to be revealed immediately */
+      isSecret: true
+      /** Optional masked value for sensitive data. */
+      secretText?: string
+      /** Controls if the masked text can be revealed */
+      canReveal?: boolean
+    }
   | {
       isSecret?: false
       secretText?: never
       canReveal?: never
     }
-  | {
-      isSecret: true
-      secretText?: string
-      canReveal?: boolean
-    }
-
-export type CodeProps = CommonProps & ConditionalProps
+)
 
 export const Code = forwardRef<HTMLButtonElement, CodeProps>(
   (
     {
+      id,
       value,
       prefix,
       className,
       isSecret,
-      secretText = DEFAULT_SECRET_TEXT,
+      secretText = "************",
       canReveal,
     }: CodeProps,
     ButtonRef
@@ -56,7 +43,7 @@ export const Code = forwardRef<HTMLButtonElement, CodeProps>(
     const [isValueVisible, setIsValueVisible] = useState(
       isSecret ? false : true
     )
-    const [copied, setCopied] = useState<CopiedStatus | null>(null)
+    const [copied, setCopied] = useState<"success" | "failed" | null>(null)
     const canCopyValue = !isSecret || (isSecret && canReveal)
 
     const toggleVisibility = async (): Promise<void> => {
@@ -66,77 +53,66 @@ export const Code = forwardRef<HTMLButtonElement, CodeProps>(
     const copyCode = async (): Promise<void> => {
       try {
         await navigator.clipboard.writeText(value)
-        setCopied(CopiedStatus.Success)
+        setCopied("success")
       } catch (err) {
-        setCopied(CopiedStatus.Failed)
+        setCopied("failed")
       }
     }
 
     useEffect(() => {
-      if (copied) {
-        setTimeout(() => {
-          setCopied(null)
-        }, COPIED_STATUS_TIMEOUT)
-      }
+      if (copied) setTimeout(() => setCopied(null), 3000)
     }, [copied])
 
     return (
       <span
+        id={id}
         className={cn(
-          "relative flex justify-start items-center w-full px-4",
+          "relative flex justify-start items-center w-full px-4 space-x-2",
           "text-sm rounded-lg",
           "bg-gray-900 text-gray-100",
           className
         )}
       >
         {prefix && <span className="mr-2 text-gray-500">{prefix}</span>}
-        <span className="relative flex-grow overflow-hidden">
-          <span className="flex py-3 max-w-full overflow-x-auto">
-            <pre className="font-mono whitespace-nowrap">
-              {isValueVisible ? value : secretText}
-            </pre>
-          </span>
+
+        <div className="relative flex-grow overflow-hidden">
+          <pre
+            className={cn(
+              "py-3 max-w-full overflow-x-auto",
+              "font-mono whitespace-nowrap"
+            )}
+          >
+            {isValueVisible ? value : secretText}
+          </pre>
+
           {/* Faded element to blend overflow for long values */}
           <span className="absolute w-8 h-full right-0 top-0 bg-gradient-to-r from-transparent via-transparent to-gray-900" />
-        </span>
+        </div>
 
-        {/* Button to toggle visibility */}
         {isSecret && (
-          <div>
-            <Button
-              variant="quiet"
-              onPress={toggleVisibility}
-              isDisabled={!canReveal}
-            >
-              <Icon
-                name={isValueVisible ? "eye-off" : "eye"}
-                size="sm"
-                className={cn({
-                  "text-gray-200 hover:text-gray-300": canReveal,
-                  "text-gray-500": !canReveal,
-                })}
-              />
-            </Button>
-          </div>
+          <Button
+            variant="quiet"
+            onPress={toggleVisibility}
+            isDisabled={!canReveal}
+            icon={isValueVisible ? "eye-off" : "eye"}
+            iconSize="sm"
+          />
         )}
 
-        {/* Button to copy code */}
-        <div className="relative justify-end ml-2">
-          <Button
-            ref={ButtonRef}
-            variant="quiet"
-            icon={
-              copied && copied === CopiedStatus.Success
-                ? "check"
-                : copied && copied === CopiedStatus.Failed
-                ? "x"
-                : "copy"
-            }
-            iconSize="sm"
-            onPress={copyCode}
-            isDisabled={!canCopyValue}
-          />
-        </div>
+        <Button
+          ref={ButtonRef}
+          variant="quiet"
+          icon={
+            copied && copied === "success"
+              ? "check"
+              : copied && copied === "failed"
+              ? "x"
+              : "copy"
+          }
+          iconSize="sm"
+          onPress={copyCode}
+          isDisabled={!canCopyValue}
+        />
       </span>
     )
   }
